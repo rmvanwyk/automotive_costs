3b52cf0c80901fba740c522edc570004b020eaf8
Riley Van Wyk <rmvanwyk@crimson.ua.edu>
2018-12-04 11:06:27 -0600

    Added functionality to compare the price efficiency of a gas vs diesel vehicle based on the mpg or each vehicle as well as the price per gallon of gas and diesel fuel.

---
+++ gas_v_diesel.py
@@ -0,0 +1,35 @@
+def miles_per_dollar(dpg, mpg):
+	return (mpg / dpg)
+
+def cost_efficiency(diesel_mpd, gas_mpd):
+	return round(((gas_mpd - diesel_mpd) / gas_mpd) * 100)
+
+gas_price = 2.5
+diesel_price = 3.21
+
+diesel_mpd = miles_per_dollar(diesel_price, 35)	
+gas_mpd = miles_per_dollar(gas_price, 29)
+print("D_mpd=" + str(diesel_mpd))
+print("G_mpd=" + str(gas_mpd))
+
+diesel_efficiency = cost_efficiency(diesel_mpd, gas_mpd)
+gasoline_efficiency = cost_efficiency(gas_mpd, diesel_mpd)
+print("D_E=%" + str(diesel_efficiency))
+print("G_E=%" + str(gasoline_efficiency))
+
+cost_efficiency_value = min(diesel_efficiency, gasoline_efficiency)
+print("CEV=%" + str(cost_efficiency_value))
+
+if (diesel_efficiency > gasoline_efficiency):
+	print("Gasoline is currently " + str(cost_efficiency(diesel_mpd, gas_mpd)) + "% more cost efficient than diesel fuel.") 
+
+	print("Diesel prices need to be at or under $" + str(((cost_efficiency_value / 100) + 1) * diesel_price) + 
+      	      " for your diesel car to be more cost efficient than a $" 
+              + str(gas_price) + " gallon of gasoline.")	
+else:
+	print("Diesel is currently " + str(cost_efficiency(gas_mpd, diesel_mpd)) + "% more cost efficient than gasoline.") 
+
+	print("Gasoline prices need to be at or under $" + str(((cost_efficiency_value / 100) + 1) * gas_price) + 
+      	      " for your gas car to be more cost efficient than a $" 
+              + str(diesel_price) + " gallon of diesel.")	
+	
